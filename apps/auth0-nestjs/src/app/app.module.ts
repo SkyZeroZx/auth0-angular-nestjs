8f33d5bf715c73e595fdf986fde8c4dafc923a3a
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2024-09-01 13:31:35 -0500

    chore(auth0-nestjs): Import feature modules in root app

--- apps/auth0-nestjs/src/app/app.module.ts
+++ apps/auth0-nestjs/src/app/app.module.ts
@@ -1,40 +1,34 @@
-import { JwtStrategy } from '@/auth/infrastructure/strategies/jwt.stragegy';
+import { AuthModule } from '@/auth';
+import { UserModule } from '@/user';
 import { AllExceptionFilter } from '@core/infrastructure/filters';
 import { HttpLoggingInterceptor } from '@core/infrastructure/interceptors';
-import {
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-  RequestMethod,
-} from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { APP_FILTER } from '@nestjs/core';
-import { PassportModule } from '@nestjs/passport';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [ AuthModule],
-  controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: APP_FILTER,
-      useClass: AllExceptionFilter,
-    },
-   ],
+	imports: [AuthModule, UserModule],
+	controllers: [AppController],
+	providers: [
+		AppService,
+		{
+			provide: APP_FILTER,
+			useClass: AllExceptionFilter
+		}
+	]
 })
 export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(HttpLoggingInterceptor)
-      .exclude(
-        // exclude to avoid logging in metric or health/documentation
-        { path: 'metrics', method: RequestMethod.GET },
-        { path: 'health', method: RequestMethod.GET },
-        { path: 'docs', method: RequestMethod.GET }
-      )
-      .forRoutes('*');
-  }
+	configure(consumer: MiddlewareConsumer) {
+		consumer
+			.apply(HttpLoggingInterceptor)
+			.exclude(
+				// exclude to avoid logging in metric or health/documentation
+				{ path: 'metrics', method: RequestMethod.GET },
+				{ path: 'health', method: RequestMethod.GET },
+				{ path: 'docs', method: RequestMethod.GET }
+			)
+			.forRoutes('*');
+	}
 }
