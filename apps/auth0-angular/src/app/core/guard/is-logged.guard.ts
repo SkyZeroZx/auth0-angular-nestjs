0a4c079c7ff36f4eada2c79dfba73b670df66881
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2024-09-05 19:51:42 -0500

    fix(auth0-angular): Fixed Guard IsLogged Angular

--- apps/auth0-angular/src/app/core/guard/is-logged.guard.ts
+++ apps/auth0-angular/src/app/core/guard/is-logged.guard.ts
@@ -1,21 +1,22 @@
 import { AuthService } from '@/services/auth';
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { Router } from '@angular/router';
 import { map, take } from 'rxjs';
 
-export function isLogged(): CanActivateFn {
-  const authService = inject(AuthService);
-  return () =>
-    authService.user$.pipe(
-      take(1),
-      map((user) => {
-        if (user) {
-          this.router.navigateByUrl('/loading');
+export function isLogged() {
+	const authService = inject(AuthService);
+	const router = inject(Router);
 
-          return false;
-        }
+	return authService.user$.pipe(
+		take(1),
+		map((user) => {
+			if (user) {
+				router.navigateByUrl('/loading');
 
-        return true;
-      })
-    );
+				return false;
+			}
+
+			return true;
+		})
+	);
 }
